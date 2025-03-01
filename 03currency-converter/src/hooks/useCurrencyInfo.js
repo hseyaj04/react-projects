88936468aa49bf2d024fcc50cd5af6d1466e26ce
Hseyaj <codex.hseyaj26@gmail.com>
2025-03-01 20:31:44 +0530

    Implemented Currency Converter using React and a fetch API for fetching Currency

---
+++ 03currency-converter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+
+const useCurrencyInfo = (currency) => {
+    let url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(url)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data);
+        
+    }, [currency])
+    console.log(data);
+    
+    return data;
+}
+
+export default useCurrencyInfo
